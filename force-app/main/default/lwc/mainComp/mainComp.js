01ec34d2ddb10c62ed0365098106c0c484752da5
roqueian84 <ryan.francis.m.roque@accenture.com>
2019-11-12 11:09:18 +0800

    test first commit

---
+++ force-app/main/default/lwc/mainComp/mainComp.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+// helloWorld.js
+import { LightningElement, track, api } from 'lwc';
+
+
+export default class mainComp extends LightningElement {
+    @track firstName = '';
+    @track lastName = '';
+    @track areDetailsVisible = true;
+    @track tcontact = [];
+    fruits = ['apple', 'banana','grapes'];
+    contacts = [
+        {
+            Id: 1,
+            Name: 'Amy Taylor',
+            Title: 'VP of Engineering',
+        },
+        {
+            Id: 2,
+            Name: 'Michael Jones',
+            Title: 'VP of Sales',
+        },
+        {
+            Id: 3,
+            Name: 'Jennifer Wu',
+            Title: 'CEO',
+        },
+    ];
+
+    handleChange(event) {
+        const field = event.target.name;
+        //const fieldlabel = event.target.label;
+        if (field === 'firstName') {
+            this.firstName = event.target.value;
+        } else if (field === 'lastName') {
+            this.lastName = event.target.value;
+        }else if (field === 'checkbox') {
+            this.areDetailsVisible = event.target.checked;
+            console.log('tick checkbox');
+        }
+    }
+    @api
+    get uppercasedFullName() {
+        const mycase = this.firstName + ' ' + this.lastName;  
+        return mycase.toUpperCase();
+    }
+    
+    handleClick(){
+
+        for(let i = 0; i < this.contacts.length; i++){
+            let vname = this.contacts[i].Name;
+            alert(vname);
+        }
+
+        //const fruits1 = this.fruits;
+        for(let i = 0; i < this.fruits.length; i++){
+            let vname = this.fruits[i];
+            alert(vname);
+        }
+
+        this.tcontact = this.contacts;
+        
+        
+    }
+}
